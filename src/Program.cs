91cfc1361c36f776a0f70468879ce069010373b6
Matthew Sawrey <matski53@gmail.com>
2022-12-04 10:54:58 +0000

    Added the day 4 solution

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,4 @@
-﻿AdventCode.ElfCalories.Task.Run();
-AdventCode.RockPaperScissors.Task.Run();
-AdventCode.RucksackReorg.Task.Run();
+﻿//AdventCode.ElfCalories.Task.Run();
+//AdventCode.RockPaperScissors.Task.Run();
+//AdventCode.RucksackReorg.Task.Run();
+AdventCode.CampCleanup.Task.Run();
