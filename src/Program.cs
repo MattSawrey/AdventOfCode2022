9c4b89dc46d3b1434dc18bccc5a5669917c6e3c2
Matthew Sawrey <matski53@gmail.com>
2022-12-11 20:49:18 +0000

    Added day 5 supply stacks solutions.

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿//AdventCode.ElfCalories.Task.Run();
 //AdventCode.RockPaperScissors.Task.Run();
 //AdventCode.RucksackReorg.Task.Run();
-AdventCode.CampCleanup.Task.Run();
+//AdventCode.CampCleanup.Task.Run();
+AdventOfCode.SupplyStacks.Task.Run();
