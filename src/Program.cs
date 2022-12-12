6b86fbfc8de8dc01a243262640a5439d07132c3c
Matthew Sawrey <matski53@gmail.com>
2022-12-12 22:29:10 +0000

    Added day 6 solution.

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
-﻿//AdventCode.ElfCalories.Task.Run();
-//AdventCode.RockPaperScissors.Task.Run();
-//AdventCode.RucksackReorg.Task.Run();
-//AdventCode.CampCleanup.Task.Run();
-AdventOfCode.SupplyStacks.Task.Run();
+﻿//AdventCode.ElfCalories.Task.Run(); // Day 1
+//AdventCode.RockPaperScissors.Task.Run(); // Day 2
+//AdventCode.RucksackReorg.Task.Run(); // Day 3
+//AdventCode.CampCleanup.Task.Run(); // Day 4
+//AdventOfCode.SupplyStacks.Task.Run(); // Day 5
+AdventOfCode.TuningTrouble.Task.Run(); // Day 6
---
+++ src/TuningTrouble/Task.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode.TuningTrouble
+{
+    public static class Task
+    {
+        public static void Run()
+        {
+            var data = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\TuningTrouble\\input.txt");
+
+            // Part 1:
+            Console.WriteLine($"Part 1: {GetIndexOfEndOfMarkerChar(data, 4)}");
+
+            // Part 2:
+            Console.WriteLine($"Part 2: {GetIndexOfEndOfMarkerChar(data, 14)}");
+        }
+
+        static int GetIndexOfEndOfMarkerChar(string data, int lengthOfMarker)
+        {
+            Queue<char> code = new Queue<char>();
+            for (int i = 0; i < lengthOfMarker-1; i++)
+                code.Enqueue(data[i]);
+
+            for (int i = lengthOfMarker-1; i < data.Length; i++)
+            {
+                code.Enqueue(data[i]);
+
+                if (code.Distinct().Count() == lengthOfMarker)
+                    return i + 1;
+
+                code.Dequeue();
+            }
+
+            throw new Exception("No marker found at all in the input string!");
+        }
+    }
+}
