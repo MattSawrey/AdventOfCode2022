77e72dd14d3a480b8818af5d52ae4cdbd4e4b5e9
Matthew Sawrey <matski53@gmail.com>
2023-04-08 12:19:57 +0100

    Added a day 4 rust solution.

--- src/Day4CampCleanup/rs/src/main.rs
+++ src/Day4CampCleanup/rs/src/main.rs
@@ -2,29 +2,81 @@ use std::env;
 use std::fs;
 
 fn main() {
-    // env::set_var("RUST_BACKTRACE", "1");
-    task1();
-    task2();
-}
-
-fn task1() {
-    println!("Task 1:");
-
-    // Get input file
     let current_dir = env::current_dir().unwrap();
     let parent_dir = current_dir.parent().unwrap();
     let file_path = format!("{}{}", parent_dir.display(), "\\input.txt");
+    let input_file_contents = fs::read_to_string(file_path).unwrap();
+    let lines = input_file_contents.lines();
+    let mut data = Vec::new();
 
-    println!(".");
+    // parse the input string to a Vec of the two int ranges represented on each line.
+    for line in lines {
+        let ranges: Vec<_> = line
+            .split(',')
+            .map(|x| {
+                x.split('-')
+                    .map(|y| y.parse::<i32>().unwrap())
+                    .collect::<Vec<_>>()
+            })
+            .collect();
+        let ranges_as_vec = ranges
+            .iter()
+            .map(|range| (range[0]..=range[1]).collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        data.push(ranges_as_vec);
+    }
+
+    let mut sum1 = 0;
+    for pair_assignment in &data {
+        let elf1 = &pair_assignment[0];
+        let elf2 = &pair_assignment[1];
+
+        //output_elf_assignment_data(&elf1, &elf2);
+
+        if intersect_vecs(elf1, elf2).len() == elf1.len()
+            || intersect_vecs(elf1, elf2).len() == elf2.len()
+        {
+            sum1 += 1;
+        }
+    }
+    println!("Challenge 1: {}", sum1);
+
+    let mut sum2 = 0;
+    for pair_assignment in &data {
+        let elf1 = &pair_assignment[0];
+        let elf2 = &pair_assignment[1];
+
+        //output_elf_assignment_data(&elf1, &elf2);
+
+        if elf1.iter().any(|&x| elf2.contains(&x)) || elf2.iter().any(|&x| elf1.contains(&x)) {
+            sum2 += 1;
+        }
+    }
+    println!("Challenge 2: {}", sum2);
 }
 
-fn task2() {
-    println!("Task 2:");
+fn output_elf_assignment_data(elf1_assignments: &Vec<i32>, elf2_assignments: &Vec<i32>) {
+    println!("Elf 1 assignment:");
+    let mut string_builder = String::new();
+    for area in elf1_assignments {
+        string_builder.push_str(&format!("{},", area));
+    }
+    println!("{}", string_builder);
 
-    // Get input file
-    let current_dir = env::current_dir().unwrap();
-    let parent_dir = current_dir.parent().unwrap();
-    let file_path = format!("{}{}", parent_dir.display(), "\\input.txt");
+    println!("Elf 2 assignment:");
+    let mut string_builder2 = String::new();
+    for area in elf2_assignments {
+        string_builder2.push_str(&format!("{},", area));
+    }
+    println!("{}", string_builder2);
+}
 
-    println!(".");
+fn intersect_vecs<T: PartialEq + Clone>(v1: &Vec<T>, v2: &Vec<T>) -> Vec<T> {
+    let mut result = Vec::new();
+    for item in v1.iter() {
+        if v2.contains(item) {
+            result.push(item.clone());
+        }
+    }
+    return result;
 }
